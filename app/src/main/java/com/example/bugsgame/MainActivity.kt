0d6fbf4c4fff4b1d442e0849827aa67d5645e05c
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2026-09-13 20:06:03 +0700

    Add gender selection radio buttons

--- app/src/main/java/com/example/bugsgame/MainActivity.kt
+++ app/src/main/java/com/example/bugsgame/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bugsgame
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editTextFullName = findViewById<EditText>(R.id.editTextFullName)
+        val radioGroupGender = findViewById<RadioGroup>(R.id.radioGroupGender)
         val buttonRegister = findViewById<Button>(R.id.buttonRegister)
 
         buttonRegister.setOnClickListener {
@@ -22,7 +24,10 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val resultMessage = "Игрок зарегистрирован:\nФИО: $fullName"
+            val selectedGenderId = radioGroupGender.checkedRadioButtonId
+            val gender = if (selectedGenderId == R.id.radioMale) "Мужской" else "Женский"
+
+            val resultMessage = "Игрок зарегистрирован:\nФИО: $fullName\nПол: $gender"
             Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show()
         }
     }
